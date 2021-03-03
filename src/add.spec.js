a15c66ac8ff7fc29ffe68b89c97e4d7108da6c1c
Kiruthika-VjShankar <kiruthikavjshankar@gmail.com>
2021-03-03 15:18:24 +0530

    Add test cases to map()

--- src/add.spec.js
+++ src/add.spec.js
@@ -10,4 +10,16 @@ describe('Add', () => {
         expect(add(1, 0)).toEqual(1);
     });
 
+    it('Sum of -11 and 11 is 0', () => {
+        expect(add(-11, 11)).toEqual(0);
+    });
+
+    it('Sum of 120 and 1 is 121', () => {
+        expect(add(120, 1)).toEqual(121);
+    });
+
+    it('Sum of 100 and 100 is 200', () => {
+        expect(add(100, 100)).toEqual(200);
+    });
+
 })
---
+++ src/map.spec.js
@@ -0,0 +1,19 @@
+const tail = require('./tail');
+
+describe('map', () => {
+
+    it('map([], cube) should give []', () => {
+        expect(map([],cube)).toEqual([]);
+    });
+
+    it('map([1,2,3], identity) should give [1,2,3]', () => {
+        expect(map([1,2,3],identity)).toEqual([1,2,3]);
+    });
+
+    it('map([1,2,3], cube) should give [1,8,27]', () => {
+        expect(map([1,2,3],cube)).toEqual([1,8,27]);
+    });
+
+    
+
+})
